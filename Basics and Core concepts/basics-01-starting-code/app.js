15fabf4f731562563166afe3504de3819fa12d0d
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2023-04-06 17:28:44 +0300

    using methods in Vue

--- Basics and Core concepts/basics-01-starting-code/app.js
+++ Basics and Core concepts/basics-01-starting-code/app.js
@@ -4,6 +4,17 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course!',
             courseLink: 'https://vuejs.org/guide/quick-start.html#using-vue-from-cdn'
         }
+    },
+    methods:{
+        randomText(){
+            const n = Math.random();
+
+            if(n < 0.5){
+                return 'Learn Vue!'
+            }else{
+                return 'Master Vue!'
+            }
+        }
     }
 })
 
