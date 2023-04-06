7a19468143b3f64499f8a06c32720bd9fa6d88f4
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2023-04-06 17:34:59 +0300

    using methods to set data in vue object

--- Basics and Core concepts/basics-01-starting-code/app.js
+++ Basics and Core concepts/basics-01-starting-code/app.js
@@ -1,18 +1,27 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
-            courseGoal: 'Finish the course!',
+            courseGoal: '',
             courseLink: 'https://vuejs.org/guide/quick-start.html#using-vue-from-cdn'
         }
     },
-    methods:{
-        randomText(){
+    methods: {
+        randomText() {
             const n = Math.random();
 
-            if(n < 0.5){
+            if (n < 0.5) {
                 return 'Learn Vue!'
-            }else{
+            } else {
                 return 'Master Vue!'
+            }
+        },
+        setCourseGoal() {
+            const n = Math.random();
+
+            if (n < 0.5) {
+               return this.courseGoal = 'Finish the course!'
+            } else {
+               return this.courseGoal = 'You are Done!'
             }
         }
     }
