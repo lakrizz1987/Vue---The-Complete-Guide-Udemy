940bd2a1597a22626f1f620536a37821e7377399
Ivaylo <ignatov.ivaylo@gmail.com>
2023-04-10 12:10:27 +0300

    conditional rendering and foreach in Vue

--- Rendering Conditional content and lists/lists-cond-01-starting-setup/app.js
+++ Rendering Conditional content and lists/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      enteredGoal:'',
+       goals: [],
+      
+    };
   },
+  methods:{
+    addGoal(){
+      this.goals.push(this.enteredGoal);
+      this.enteredGoal = '';
+    }
+  }
 });
 
 app.mount('#user-goals');
