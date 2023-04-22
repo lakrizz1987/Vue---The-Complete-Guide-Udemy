f8956265874b90609cac083e920ed44d0ba66c09
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2023-04-22 12:37:53 +0300

    add toggle function

--- Intro to Components/cmp-intro-01-starting-setup/app.js
+++ Intro to Components/cmp-intro-01-starting-setup/app.js
@@ -1,12 +1,12 @@
 const app = Vue.createApp({
 });
 
-app.component('friends-component',{
+app.component('friends-component', {
     template: `
-    <li v-for="friend of friends">
+    <li>
           <h2>{{friend.name}}</h2>
-          <button>Show Details</button>
-          <ul>
+          <button @click="toggleDetails">Show Details</button>
+          <ul v-show="toggle">
             <li><strong>Phone:</strong>{{friend.phone}}</li>
             <li><strong>Email:</strong>{{friend.email}}</li>
           </ul>
@@ -15,10 +15,14 @@ app.component('friends-component',{
     `,
     data() {
         return {
-            friends: [
+            friend:
                 { id: '1', name: 'Ivaylo', phone: 0888996547, email: 'ivo@foo.com' },
-                { id: '1', name: 'Pesho', phone: 088899447777, email: 'peshkata@foo.com' },
-            ]
+            toggle: false
+        }
+    },
+    methods: {
+        toggleDetails(){
+            this.toggle = !this.toggle;
         }
     }
 });
