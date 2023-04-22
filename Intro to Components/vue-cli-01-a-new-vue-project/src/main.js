e466e18fdb9c4f0e4f0ce718034943b1fa1e9d01
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2023-04-22 20:16:53 +0300

    create FriendList.vue

--- Intro to Components/vue-cli-01-a-new-vue-project/src/main.js
+++ Intro to Components/vue-cli-01-a-new-vue-project/src/main.js
@@ -1,3 +1,8 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+import FriendList from './Components/FriendList.vue'
 
-createApp({}).mount('');
+const app = createApp(App)
+app.component('friend-list',FriendList);
+
+app.mount('#app');
